de4690bdc4c42c6c6c4a86c1b7540e0dec46b5a2
ButHit <31693372+ButHit@users.noreply.github.com>
2017-09-11 13:57:34 +0800

    add some bobbby

--- app/src/main/java/com/example/kinchun/reallynew/MainActivity.java
+++ app/src/main/java/com/example/kinchun/reallynew/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is not really master
-        String bobby = "I am Bobby Dog" ;
-        int bobbyho = 9;
+        //This is mybranch
     }
 }
