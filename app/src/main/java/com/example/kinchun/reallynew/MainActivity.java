1f8924c1e85acd885e2c56b1b0040767b23f7863
bobbyho2017 <bobbyho2008@gmail.com>
2017-09-11 13:41:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kinchun/reallynew/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kinchun.reallynew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //This is master
+    }
+}
